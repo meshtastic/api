0ba1a9fa36dba4498a80f5b91a1699e490a495f4
Sacha Weatherstone <sachaw100@hotmail.com>
2022-12-10 16:42:44 +1000

    add missing file

---
+++ src/routes/github.ts
@@ -0,0 +1,27 @@
+import { app } from '../index.js';
+import {
+  DeviceFirmwareResource,
+  deviceOctokit,
+  DeviceRequestOptions,
+} from '../utils/github.js';
+
+export const GithubRoutes = () => {
+  return app.get("/github/releases", async (req, res) => {
+    const releases = await deviceOctokit.rest.repos.listReleases(
+      DeviceRequestOptions
+    );
+
+    res.send(
+      releases.data.map((release) => {
+        return <DeviceFirmwareResource>{
+          id: release.tag_name,
+          title: release.name,
+          page_url: release.html_url,
+          zip_url: release.assets.find((asset) =>
+            asset.name.startsWith("firmware-")
+          )?.browser_download_url,
+        };
+      })
+    );
+  });
+};
