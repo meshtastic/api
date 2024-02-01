1a39402127d40cee1e6277f4356387c7519ef41b
Sacha Weatherstone <sachaw100@hotmail.com>
2024-02-01 16:54:15 +1000

    add type annotation

--- src/services/gateway.ts
+++ src/services/gateway.ts
@@ -4,6 +4,7 @@ import { GatewayStreamResponse } from "@buf/meshtastic_api.bufbuild_es/protobufs
 import type { GatewayService } from "@buf/meshtastic_api.connectrpc_es/protobufs/gateway/v1/gateway_service_connect.js";
 import { prisma } from "../lib/index.js";
 import { Timestamp } from "@bufbuild/protobuf";
+import { Channel } from "@prisma/client";
 
 export class Gateway implements ServiceImpl<typeof GatewayService> {
   public async *gatewayStream(): AsyncGenerator<GatewayStreamResponse> {
@@ -31,7 +32,7 @@ export class Gateway implements ServiceImpl<typeof GatewayService> {
           longitude: gateway.longitude ?? undefined,
           createdAt: Timestamp.fromDate(gateway.createdAt),
           updatedAt: Timestamp.fromDate(gateway.updatedAt),
-          channels: gateway.channels.map((channel) => {
+          channels: gateway.channels.map((channel: Channel) => {
             return {
               id: channel.id,
               name: channel.name,
