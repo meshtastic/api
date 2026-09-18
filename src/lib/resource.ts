6d54be9d76077c9ceabc88b81d31ddeaae451286
Ben Meadors <benmmeadors@gmail.com>
2026-09-18 13:40:25 -0500

    Update resource.ts

--- src/lib/resource.ts
+++ src/lib/resource.ts
@@ -15,7 +15,7 @@ export interface DeviceHardware {
   url?: string; // Override URL for the device (for actively supported devices only)
   key?: string; // Optional key to differentiate multiple entries for the same hwModel
   variant?: string; // Optional variant to differentiate multiple entries for the same hwModel
-  isMaker?: bool; // Optional indicator for maker tier 
+  isMaker: bool; // Optional indicator for maker tier 
 }
 
 export const deviceHardwareList: DeviceHardware[] = [
